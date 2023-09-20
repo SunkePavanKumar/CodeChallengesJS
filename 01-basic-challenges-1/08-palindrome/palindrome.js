38cd555f6fbf694195d0c248d8c6e33c46507b6d
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2023-09-20 14:25:44 +0530

    Solved the 10 challenges

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,8 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+let reversedStr = str.split("").reverse().join("")
+if(str === reversedStr) return true
+return false
+}
 
 module.exports = isPalindrome;
